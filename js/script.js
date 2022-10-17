57b8b73df9b1ae7ce31e6d36b1093fb0d2bb13c7
Varesva <varesva.dev@gmail.com>
2022-10-17 21:41:17 +0200

    while loop on items

--- js/script.js
+++ js/script.js
@@ -1,49 +1,30 @@
 const itemContainer = document.querySelector('#item-container');
 
 function loopingNb() {
-    for (i = 1; i <= 3; i++) {
-        // console.log(i);
-        // return {...i};
+    let i = 1;
+
+    while (i <= 13) {
+        const items = [
+                {
+                    name: 'Plate 0' + i,
+                    price: Math.floor(Math.random() * (40 - 20) + 20),
+                    image: `./img/item/items (${i}).jpg`
+                },
+        ];
+        
+        items.forEach(product => {
+            itemContainer.innerHTML += 
+            `<div class="card">
+                <img class="img-card" src="${product.image}">
+                <div class="text-card">
+                    <p class="title"><a href="#"> ${product.name}</a></p>
+                    <p class="price"><a href="#"> ${product.price} € </a></p>
+                </div>
+            </div>`
+        });
+
+        i++;
     };
-    return i;
 }
 
-function allItems() {
-    loopingNb();
-    items = [
-            {
-                name: 'Plate 0' + i,
-                price: Math.floor(Math.random() * (40 - 20) + 20),
-                image: `./img/item/items (${i}).jpg`
-            },
-    ];
-    
-    items.forEach(product => {
-        itemContainer.innerHTML += `
-        <div class="card">
-            <img class="img-card" src="${product.image}">
-            <div class="text-card">
-                <p class="title"><a href="#"> ${product.name}</a></p>
-                <p class="price"><a href="#"> ${product.price} € </a></p>
-            </div>
-        </div>`
-    });
-}
-
-allItems();
-
- 
-
-
-
-// items.forEach(product => {
-//     itemContainer.innerHTML += `
-//     <div class="card">
-//         <img class="img-card" src="${product.image}">
-//         <div class="text-card">
-//             <p class="title"><a href="#"> ${product.name}</a></p>
-//             <p class="price"><a href="#"> ${product.price} € </a></p>
-//         </div>
-//     </div>`
-// });
-
+loopingNb();
